568973c65b2714b2925b979ab108bc738f7916ab
deluxe1225 <Fabao1225@gmail.com>
2025-08-23 10:06:09 +0100

    snapshot

--- leaderboard.js
+++ leaderboard.js
@@ -1,12 +1,85 @@
 window.LEADERBOARD = {
-  "updatedAt": "2025-08-23 10:00",
+  "updatedAt": "2025-08-23 10:06",
   "players": [
+    {
+      "user": "santosss1_0",
+      "level": 3,
+      "xp": 43,
+      "nextXp": 125,
+      "totalXp": 243,
+      "badges": [],
+      "avatar": ""
+    },
+    {
+      "user": "Anonymous",
+      "level": 2,
+      "xp": 50,
+      "nextXp": 112,
+      "totalXp": 150,
+      "badges": [
+        "sub"
+      ],
+      "avatar": ""
+    },
+    {
+      "user": "fsantos88",
+      "level": 2,
+      "xp": 50,
+      "nextXp": 112,
+      "totalXp": 150,
+      "badges": [],
+      "avatar": ""
+    },
+    {
+      "user": "kappag4mer",
+      "level": 2,
+      "xp": 50,
+      "nextXp": 112,
+      "totalXp": 150,
+      "badges": [
+        "sub"
+      ],
+      "avatar": ""
+    },
+    {
+      "user": "mr__slean",
+      "level": 2,
+      "xp": 50,
+      "nextXp": 112,
+      "totalXp": 150,
+      "badges": [
+        "sub"
+      ],
+      "avatar": ""
+    },
+    {
+      "user": "trickytrickydale",
+      "level": 2,
+      "xp": 50,
+      "nextXp": 112,
+      "totalXp": 150,
+      "badges": [
+        "sub"
+      ],
+      "avatar": ""
+    },
+    {
+      "user": "kassandra_c96",
+      "level": 1,
+      "xp": 42,
+      "nextXp": 100,
+      "totalXp": 42,
+      "badges": [
+        "sub"
+      ],
+      "avatar": ""
+    },
     {
       "user": "fablopss",
       "level": 1,
-      "xp": 23,
+      "xp": 34,
       "nextXp": 100,
-      "totalXp": 23,
+      "totalXp": 34,
       "badges": [
         "sub",
         "mod"
@@ -16,12 +89,21 @@ window.LEADERBOARD = {
     {
       "user": "a_saika",
       "level": 1,
-      "xp": 8,
+      "xp": 21,
       "nextXp": 100,
-      "totalXp": 8,
+      "totalXp": 21,
       "badges": [
         "mod"
       ],
+      "avatar": ""
+    },
+    {
+      "user": "znull17",
+      "level": 1,
+      "xp": 20,
+      "nextXp": 100,
+      "totalXp": 20,
+      "badges": [],
       "avatar": ""
     }
   ]
