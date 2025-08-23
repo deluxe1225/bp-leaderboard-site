50683f89878649f174c0847cb47fc096bdd91549
deluxe1225 <Fabao1225@gmail.com>
2025-08-23 09:55:01 +0100

    snapshot

--- leaderboard.js
+++ leaderboard.js
@@ -1,12 +1,12 @@
 window.LEADERBOARD = {
-  "updatedAt": "2025-08-23 09:20",
+  "updatedAt": "2025-08-23 09:47",
   "players": [
     {
       "user": "fablopss",
       "level": 1,
-      "xp": 14,
+      "xp": 23,
       "nextXp": 100,
-      "totalXp": 14,
+      "totalXp": 23,
       "badges": [
         "sub",
         "mod"
