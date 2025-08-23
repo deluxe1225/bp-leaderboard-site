ce9a243b5976a53692888dfe72a47547a42309c8
deluxe1225 <Fabao1225@gmail.com>
2025-08-23 09:42:17 +0100

    primeiro deploy

---
+++ leaderboard.js
@@ -0,0 +1,28 @@
+window.LEADERBOARD = {
+  "updatedAt": "2025-08-23 09:20",
+  "players": [
+    {
+      "user": "fablopss",
+      "level": 1,
+      "xp": 14,
+      "nextXp": 100,
+      "totalXp": 14,
+      "badges": [
+        "sub",
+        "mod"
+      ],
+      "avatar": ""
+    },
+    {
+      "user": "a_saika",
+      "level": 1,
+      "xp": 8,
+      "nextXp": 100,
+      "totalXp": 8,
+      "badges": [
+        "mod"
+      ],
+      "avatar": ""
+    }
+  ]
+};
