821fd9ce853e2e4513df7458f43928cc0a36135e
deluxe1225 <Fabao1225@gmail.com>
2025-08-24 18:19:33 +0100

    snapshot

--- leaderboard.js
+++ leaderboard.js
@@ -1,16 +1,25 @@
 window.LEADERBOARD = {
-  "updatedAt": "2025-08-23 23:12",
+  "updatedAt": "2025-08-24 18:19",
   "players": [
     {
       "user": "fablopss",
-      "level": 14,
-      "xp": 58,
-      "nextXp": 436,
-      "totalXp": 1358,
+      "level": 16,
+      "xp": 310,
+      "nextXp": 547,
+      "totalXp": 1810,
       "badges": [
         "sub",
         "mod"
       ],
+      "avatar": ""
+    },
+    {
+      "user": "lixianisredcoat",
+      "level": 14,
+      "xp": 87,
+      "nextXp": 436,
+      "totalXp": 1387,
+      "badges": [],
       "avatar": ""
     },
     {
@@ -22,6 +31,53 @@ window.LEADERBOARD = {
       "badges": [
         "sub"
       ],
+      "avatar": ""
+    },
+    {
+      "user": "brunalavrador",
+      "level": 3,
+      "xp": 48,
+      "nextXp": 125,
+      "totalXp": 248,
+      "badges": [],
+      "avatar": ""
+    },
+    {
+      "user": "lreclaim",
+      "level": 3,
+      "xp": 48,
+      "nextXp": 125,
+      "totalXp": 248,
+      "badges": [
+        "vip"
+      ],
+      "avatar": ""
+    },
+    {
+      "user": "luc3svenge",
+      "level": 3,
+      "xp": 48,
+      "nextXp": 125,
+      "totalXp": 248,
+      "badges": [],
+      "avatar": ""
+    },
+    {
+      "user": "tiagobrazona25",
+      "level": 3,
+      "xp": 48,
+      "nextXp": 125,
+      "totalXp": 248,
+      "badges": [],
+      "avatar": ""
+    },
+    {
+      "user": "xx_backthis",
+      "level": 3,
+      "xp": 48,
+      "nextXp": 125,
+      "totalXp": 248,
+      "badges": [],
       "avatar": ""
     },
     {
@@ -56,6 +112,26 @@ window.LEADERBOARD = {
       "avatar": ""
     },
     {
+      "user": "fsantos88",
+      "level": 2,
+      "xp": 55,
+      "nextXp": 112,
+      "totalXp": 155,
+      "badges": [
+        "vip"
+      ],
+      "avatar": ""
+    },
+    {
+      "user": "trickytrickydale",
+      "level": 2,
+      "xp": 55,
+      "nextXp": 112,
+      "totalXp": 155,
+      "badges": [],
+      "avatar": ""
+    },
+    {
       "user": "bakardipt",
       "level": 2,
       "xp": 50,
@@ -64,15 +140,6 @@ window.LEADERBOARD = {
       "badges": [
         "sub"
       ],
-      "avatar": ""
-    },
-    {
-      "user": "fsantos88",
-      "level": 2,
-      "xp": 50,
-      "nextXp": 112,
-      "totalXp": 150,
-      "badges": [],
       "avatar": ""
     },
     {
@@ -88,17 +155,6 @@ window.LEADERBOARD = {
     },
     {
       "user": "mr__slean",
-      "level": 2,
-      "xp": 50,
-      "nextXp": 112,
-      "totalXp": 150,
-      "badges": [
-        "sub"
-      ],
-      "avatar": ""
-    },
-    {
-      "user": "trickytrickydale",
       "level": 2,
       "xp": 50,
       "nextXp": 112,
@@ -129,6 +185,15 @@ window.LEADERBOARD = {
       "avatar": ""
     },
     {
+      "user": "sympathetichalf_lifeavexz",
+      "level": 1,
+      "xp": 35,
+      "nextXp": 100,
+      "totalXp": 35,
+      "badges": [],
+      "avatar": ""
+    },
+    {
       "user": "a_Saika",
       "level": 1,
       "xp": 25,
@@ -152,6 +217,325 @@ window.LEADERBOARD = {
       "xp": 20,
       "nextXp": 100,
       "totalXp": 20,
+      "badges": [],
+      "avatar": ""
+    },
+    {
+      "user": "edugamercr7",
+      "level": 1,
+      "xp": 10,
+      "nextXp": 100,
+      "totalXp": 10,
+      "badges": [],
+      "avatar": ""
+    },
+    {
+      "user": "russo2k22",
+      "level": 1,
+      "xp": 10,
+      "nextXp": 100,
+      "totalXp": 10,
+      "badges": [],
+      "avatar": ""
+    },
+    {
+      "user": "ahnuc87",
+      "level": 1,
+      "xp": 5,
+      "nextXp": 100,
+      "totalXp": 5,
+      "badges": [],
+      "avatar": ""
+    },
+    {
+      "user": "ana_de_marvao",
+      "level": 1,
+      "xp": 5,
+      "nextXp": 100,
+      "totalXp": 5,
+      "badges": [],
+      "avatar": ""
+    },
+    {
+      "user": "apricitypt",
+      "level": 1,
+      "xp": 5,
+      "nextXp": 100,
+      "totalXp": 5,
+      "badges": [],
+      "avatar": ""
+    },
+    {
+      "user": "bosingwa83_pt",
+      "level": 1,
+      "xp": 5,
+      "nextXp": 100,
+      "totalXp": 5,
+      "badges": [],
+      "avatar": ""
+    },
+    {
+      "user": "d4nizqy",
+      "level": 1,
+      "xp": 5,
+      "nextXp": 100,
+      "totalXp": 5,
+      "badges": [
+        "vip"
+      ],
+      "avatar": ""
+    },
+    {
+      "user": "derisivecrap4gosq3r",
+      "level": 1,
+      "xp": 5,
+      "nextXp": 100,
+      "totalXp": 5,
+      "badges": [],
+      "avatar": ""
+    },
+    {
+      "user": "dj_manuel_12",
+      "level": 1,
+      "xp": 5,
+      "nextXp": 100,
+      "totalXp": 5,
+      "badges": [],
+      "avatar": ""
+    },
+    {
+      "user": "emanuelpaulojsks2",
+      "level": 1,
+      "xp": 5,
+      "nextXp": 100,
+      "totalXp": 5,
+      "badges": [],
+      "avatar": ""
+    },
+    {
+      "user": "empire_gaming_tv",
+      "level": 1,
+      "xp": 5,
+      "nextXp": 100,
+      "totalXp": 5,
+      "badges": [],
+      "avatar": ""
+    },
+    {
+      "user": "fringedcliffmmelcny",
+      "level": 1,
+      "xp": 5,
+      "nextXp": 100,
+      "totalXp": 5,
+      "badges": [],
+      "avatar": ""
+    },
+    {
+      "user": "instintosilver",
+      "level": 1,
+      "xp": 5,
+      "nextXp": 100,
+      "totalXp": 5,
+      "badges": [],
+      "avatar": ""
+    },
+    {
+      "user": "jayoak",
+      "level": 1,
+      "xp": 5,
+      "nextXp": 100,
+      "totalXp": 5,
+      "badges": [
+        "vip"
+      ],
+      "avatar": ""
+    },
+    {
+      "user": "katchmetv",
+      "level": 1,
+      "xp": 5,
+      "nextXp": 100,
+      "totalXp": 5,
+      "badges": [
+        "sub"
+      ],
+      "avatar": ""
+    },
+    {
+      "user": "knight_rider_hd",
+      "level": 1,
+      "xp": 5,
+      "nextXp": 100,
+      "totalXp": 5,
+      "badges": [],
+      "avatar": ""
+    },
+    {
+      "user": "lu_santos1212",
+      "level": 1,
+      "xp": 5,
+      "nextXp": 100,
+      "totalXp": 5,
+      "badges": [],
+      "avatar": ""
+    },
+    {
+      "user": "mauromauricio93",
+      "level": 1,
+      "xp": 5,
+      "nextXp": 100,
+      "totalXp": 5,
+      "badges": [
+        "vip"
+      ],
+      "avatar": ""
+    },
+    {
+      "user": "meramentebia",
+      "level": 1,
+      "xp": 5,
+      "nextXp": 100,
+      "totalXp": 5,
+      "badges": [],
+      "avatar": ""
+    },
+    {
+      "user": "miigzsama",
+      "level": 1,
+      "xp": 5,
+      "nextXp": 100,
+      "totalXp": 5,
+      "badges": [
+        "vip"
+      ],
+      "avatar": ""
+    },
+    {
+      "user": "pardalgod",
+      "level": 1,
+      "xp": 5,
+      "nextXp": 100,
+      "totalXp": 5,
+      "badges": [],
+      "avatar": ""
+    },
+    {
+      "user": "pedro19812",
+      "level": 1,
+      "xp": 5,
+      "nextXp": 100,
+      "totalXp": 5,
+      "badges": [
+        "sub",
+        "mod"
+      ],
+      "avatar": ""
+    },
+    {
+      "user": "possessivopk",
+      "level": 1,
+      "xp": 5,
+      "nextXp": 100,
+      "totalXp": 5,
+      "badges": [],
+      "avatar": ""
+    },
+    {
+      "user": "rickygabriel_msi",
+      "level": 1,
+      "xp": 5,
+      "nextXp": 100,
+      "totalXp": 5,
+      "badges": [
+        "mod"
+      ],
+      "avatar": ""
+    },
+    {
+      "user": "san7izin",
+      "level": 1,
+      "xp": 5,
+      "nextXp": 100,
+      "totalXp": 5,
+      "badges": [],
+      "avatar": ""
+    },
+    {
+      "user": "silvaaontwitch",
+      "level": 1,
+      "xp": 5,
+      "nextXp": 100,
+      "totalXp": 5,
+      "badges": [],
+      "avatar": ""
+    },
+    {
+      "user": "snowfireseven",
+      "level": 1,
+      "xp": 5,
+      "nextXp": 100,
+      "totalXp": 5,
+      "badges": [
+        "sub",
+        "mod"
+      ],
+      "avatar": ""
+    },
+    {
+      "user": "sobraesperta766",
+      "level": 1,
+      "xp": 5,
+      "nextXp": 100,
+      "totalXp": 5,
+      "badges": [],
+      "avatar": ""
+    },
+    {
+      "user": "sol_ttv",
+      "level": 1,
+      "xp": 5,
+      "nextXp": 100,
+      "totalXp": 5,
+      "badges": [],
+      "avatar": ""
+    },
+    {
+      "user": "therealjackoom",
+      "level": 1,
+      "xp": 5,
+      "nextXp": 100,
+      "totalXp": 5,
+      "badges": [],
+      "avatar": ""
+    },
+    {
+      "user": "thisisnotmyjob",
+      "level": 1,
+      "xp": 5,
+      "nextXp": 100,
+      "totalXp": 5,
+      "badges": [
+        "vip"
+      ],
+      "avatar": ""
+    },
+    {
+      "user": "wzbot",
+      "level": 1,
+      "xp": 5,
+      "nextXp": 100,
+      "totalXp": 5,
+      "badges": [
+        "mod"
+      ],
+      "avatar": ""
+    },
+    {
+      "user": "xotinha_731",
+      "level": 1,
+      "xp": 5,
+      "nextXp": 100,
+      "totalXp": 5,
       "badges": [],
       "avatar": ""
     },
